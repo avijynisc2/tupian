f68e6f9c6d0b365c16215ad0001016228740e3b6
X-eusao <ggent27@hotmail.com>
2025-03-31 00:37:15 +0800

    Update FooterComponent.vue

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -1,18 +1,6 @@
 <template>
   <footer class="sm:mt-12 mb-2 text-center text-xs sm:text-sm">
     <div class="flex flex-wrap justify-center gap-x-2 gap-y-1">
-      <span>富强</span>
-      <span>民主</span>
-      <span>文明</span>
-      <span>和谐</span>
-      <span>自由</span>
-      <span>平等</span>
-      <span>公正</span>
-      <span>法治</span>
-      <span>爱国</span>
-      <span>敬业</span>
-      <span>诚信</span>
-      <span>友善</span>
     </div>
     <div class="mt-2 space-x-2">
       <span>© 2024</span>
